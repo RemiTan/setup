dcabd344a6ab75058d727c15374c126cdb70c305
remi <remi.tan@wintics.com>
2025-01-14 14:29:50 +0100

    add: new branc

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,6 +2,9 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 -- local capabilities = require("blink.cmp").get_lsp_capabilities()
+require("mason").setup {
+  ensure_installed = { "pyright", "ruff", "lua_ls", "basedpyright" }, -- Add your desired servers here
+}
 
 local lspconfig = require "lspconfig"
 
--- lua/configs/snacks.lua
+++ lua/configs/snacks.lua
@@ -320,75 +320,8 @@ local dashboard = {
   },
   sections = {
     { section = "header" },
-    {
-      pane = 2,
-      section = "terminal",
-      cmd = "colorscript -e square",
-      height = 5,
-      padding = 1,
-    },
-    {
-      pane = 2,
-      icon = " ",
-      desc = "Browse Repo",
-      padding = 1,
-      key = "b",
-      action = function()
-        Snacks.gitbrowse()
-      end,
-    },
     { section = "keys", gap = 1, padding = 2 },
     { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-    {
-      pane = 2,
-      icon = " ",
-      section = "terminal",
-      enabled = function()
-        return Snacks.git.get_root() ~= nil
-      end,
-      padding = 1,
-      ttl = 5 * 60,
-      indent = 3,
-      title = "Notifications",
-      cmd = "gh notify -s -a -n5",
-      action = function()
-        vim.ui.open "https://github.com/notifications"
-      end,
-      key = "n",
-      height = 7,
-    },
-    {
-      pane = 2,
-      icon = " ",
-      title = "Git Status",
-      section = "terminal",
-      enabled = function()
-        return Snacks.git.get_root() ~= nil
-      end,
-      cmd = "git --no-pager diff --stat -B -M -C",
-      height = 5,
-      padding = 1,
-      ttl = 5 * 60,
-      indent = 3,
-    },
-    {
-      pane = 2,
-      section = "terminal",
-      enabled = function()
-        return Snacks.git.get_root() ~= nil
-      end,
-      padding = 1,
-      ttl = 5 * 60,
-      indent = 3,
-      icon = " ",
-      title = "Open PRs",
-      cmd = "gh pr list -L 3",
-      key = "p",
-      action = function()
-        vim.fn.jobstart("gh pr list --web", { detach = true })
-      end,
-      height = 7,
-    },
     { section = "startup" },
   },
 }
